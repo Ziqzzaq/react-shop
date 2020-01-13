131cc211455b8e042d35cf68373bfaab83c70100
Michał Borzęcki <michal.borzecki@britenet.com.pl>
2020-01-13 14:17:42 +0100

    create cart-item component and connected our cart dropdown with cartItems reducer

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -2,13 +2,28 @@ import React from 'react';
 import Button from '../button/button.component';
 
 import './cart-dropdown.styles.scss';
+import { connect } from 'react-redux';
+import { Item } from '../../models/state/ShopState';
+import CartItem from '../cart-item/cart-item.component';
+import { AppState } from '../../models/state/AppState';
 
-const CartDropdown = () => (
+interface CartDropdownProps {
+    cartItems: Item[];
+}
+
+const CartDropdown = ({ cartItems }: CartDropdownProps) => (
     <div className="cart-dropdown">
         <div className="cart-items">
+            {cartItems.map((cartItem) => (
+                <CartItem key={cartItem.id} item={cartItem} />
+            ))}
             <Button inverted>GO TO CHECKOUT</Button>
         </div>
     </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({ cartState }: AppState) => ({
+    cartItems: cartState.cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
---
+++ src/components/cart-item/cart-item.component.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './cart-item.styles.scss';
+import { Item } from '../../models/state/ShopState';
+
+interface CartItemProps {
+    item: Item;
+}
+
+const CartItem = ({ item: { imageUrl, price, name, quantity } }: CartItemProps) => (
+    <div className="cart-item">
+        <img src={imageUrl} alt="item" />
+        <div className="item-details">
+            <span className="name">{name}</span>
+            <span className="price">
+                {quantity} x {price}
+            </span>
+        </div>
+    </div>
+);
+
+export default CartItem;
